8463991df3d2e5e6d1bd7ca80d4ec67200b851b5
Durgesh Kumar Dwivedi <durgeshdwivedi81@gmail.com>
2024-01-21 21:29:23 +0530

    Remove unused import statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar} from 'react-native';
-import tw from 'twrnc';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
